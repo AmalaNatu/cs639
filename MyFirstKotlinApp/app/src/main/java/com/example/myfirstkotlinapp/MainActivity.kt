ef1214c09f6a419f7f3618138b7031249c8d0bd1
AmalaNatu <amalanatu2110@gmail.com>
2023-11-04 11:43:43 -0400

    commit for hw4

---
+++ MyFirstKotlinApp/app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myfirstkotlinapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import com.example.myfirstkotlinapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val welcomeMessage = findViewById<TextView>(R.id.welcomeMessage)
+        val name = "Your Name" // Replace with your name
+
+        welcomeMessage.text = "Hello Android $name"
+    }
+}
